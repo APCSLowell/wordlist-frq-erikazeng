8d91812b26297992b1ea0d9dfde382c2d063c827
erikazeng <143123644+erikazeng@users.noreply.github.com>
2024-04-04 11:40:59 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,7 +11,7 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     int k =0; 
-    for(int i=0; i<<myList.size(); i++){
+    for(int i=0; i<myList.size(); i++){
       if(myList.get(i).length == len){
         k++;
       }
